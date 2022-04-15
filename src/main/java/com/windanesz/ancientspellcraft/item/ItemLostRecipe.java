9f656da4bd6959674b7076a3a786a0d5d7be28c1
WinDanesz <31292708+WinDanesz@users.noreply.github.com>
2022-04-16 01:52:12 +0200

    fix: Fixed Purifying Elixir's artefact recipe

--- src/main/java/com/windanesz/ancientspellcraft/item/ItemLostRecipe.java
+++ src/main/java/com/windanesz/ancientspellcraft/item/ItemLostRecipe.java
@@ -2,7 +2,6 @@ package com.windanesz.ancientspellcraft.item;
 
 import net.minecraft.item.EnumRarity;
 import net.minecraft.item.Item;
-import net.minecraft.item.ItemStack;
 
 public class ItemLostRecipe extends ItemASArtefact {
 	public ItemLostRecipe(EnumRarity rarity, Type type) {
@@ -11,20 +10,12 @@ public class ItemLostRecipe extends ItemASArtefact {
 	}
 
 	@Override
-	public boolean hasContainerItem(ItemStack stack) {
+	public boolean hasContainerItem() {
 		return true;
 	}
 
-	/**
-	 * The item will return itself when used in a crafting recipe
-	 */
 	@Override
 	public Item getContainerItem() {
 		return this;
-	}
-
-	@Override
-	public ItemStack getContainerItem(ItemStack itemStack) {
-		return itemStack;
 	}
 }
