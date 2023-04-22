fb185158f21fda244c7a25cac71a0f36f972decf
WinDanesz <31292708+WinDanesz@users.noreply.github.com>
2023-04-22 19:00:47 +0200

    feat: Small buff to Candlelight's light level

--- src/main/java/com/windanesz/ancientspellcraft/block/BlockCandleLight.java
+++ src/main/java/com/windanesz/ancientspellcraft/block/BlockCandleLight.java
@@ -22,7 +22,7 @@ public class BlockCandleLight extends Block implements ITileEntityProvider {
 		super(Material.AIR);
 		setDefaultState(blockState.getBaseState());
 		setTickRandomly(false);
-		setLightLevel(0.7F);
+		setLightLevel(0.75F);
 		setCreativeTab(null);
 	}
 
