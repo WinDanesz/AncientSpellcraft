0d3d49a6915d2947641acb9851cc8815d4c3473d
WinDanesz <31292708+WinDanesz@users.noreply.github.com>
2025-03-03 21:06:22 +0100

    fix: Fix compat with SpecialMobs. Fixes #270, fixes #265

--- src/main/java/com/windanesz/ancientspellcraft/mixin/minecraft/MixinIsEntityUndead.java
+++ src/main/java/com/windanesz/ancientspellcraft/mixin/minecraft/MixinIsEntityUndead.java
@@ -1,6 +1,7 @@
 package com.windanesz.ancientspellcraft.mixin.minecraft;
 
 import net.minecraft.entity.EntityLivingBase;
+import net.minecraft.potion.Potion;
 import net.minecraft.util.ResourceLocation;
 import net.minecraftforge.fml.common.registry.ForgeRegistries;
 import org.spongepowered.asm.mixin.Mixin;
@@ -13,13 +14,18 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 @Mixin(EntityLivingBase.class)
 public abstract class MixinIsEntityUndead {
 
-	@Shadow public abstract boolean isEntityUndead();
+	@Shadow
+	public abstract boolean isEntityUndead();
 
 	@Unique
 	private static ResourceLocation CURSE_OF_UNDEATH = new ResourceLocation("ebwizardry:curse_of_undeath");
-	
+
 	@Inject(method = "isEntityUndead", at = @At("RETURN"), cancellable = true)
 	public void modifyUndeadStatus(CallbackInfoReturnable<Boolean> cir) {
-		cir.setReturnValue(((EntityLivingBase) (Object) this).isPotionActive(ForgeRegistries.POTIONS.getValue(CURSE_OF_UNDEATH) )|| cir.getReturnValue());
+		Potion potion = ForgeRegistries.POTIONS.getValue(CURSE_OF_UNDEATH);
+		if (((EntityLivingBase) (Object) this).ticksExisted != 0 && potion != null) {
+			//  net.minecraft.entity.EntityLivingBase.activePotionsMap is not yet initialized when the entity is not yet constructed
+			cir.setReturnValue(((EntityLivingBase) (Object) this).isPotionActive(potion) || cir.getReturnValue());
+		}
 	}
 }
