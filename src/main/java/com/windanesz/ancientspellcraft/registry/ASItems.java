7024482f518ac7b211c73349999694a18a3b00ab
WinDanesz <31292708+WinDanesz@users.noreply.github.com>
2025-06-11 01:25:11 +0200

    progress

--- src/main/java/com/windanesz/ancientspellcraft/registry/ASItems.java
+++ src/main/java/com/windanesz/ancientspellcraft/registry/ASItems.java
@@ -200,6 +200,7 @@ public final class ASItems {
 	public static final Item charm_pale_flame = placeholder();
 	public static final Item charm_arcane_mirror = placeholder();
 	public static final Item charm_infernal_stone = placeholder();
+	public static final Item charm_meteorite_stone = placeholder();
 	public static final Item charm_transmutation_cube = placeholder();
 	public static final Item charm_ancient_wand = placeholder();
 	public static final Item charm_specterlight_torch = placeholder();
@@ -797,6 +798,7 @@ public final class ASItems {
 		ItemRegistry.registerItemArtefact(registry, "charm_crystal_sickle", AncientSpellcraft.MODID, new ItemCrystalSickle(EnumRarity.RARE, ItemArtefact.Type.CHARM));
 		ItemRegistry.registerItemArtefact(registry, "charm_focus_stone", AncientSpellcraft.MODID, new ItemFocusStone(EnumRarity.RARE, ItemArtefact.Type.CHARM));
 		ItemRegistry.registerItemArtefact(registry, "charm_infernal_stone", AncientSpellcraft.MODID, new ItemASArtefact(EnumRarity.EPIC, ItemArtefact.Type.CHARM));
+		ItemRegistry.registerItemArtefact(registry, "charm_meteorite_stone", AncientSpellcraft.MODID, new ItemASArtefact(EnumRarity.RARE, ItemArtefact.Type.CHARM));
 		registerItemBlock(registry, ASBlocks.ARTEFACT_PENSIVE, new ItemBlock(ASBlocks.ARTEFACT_PENSIVE) {
 			@Override
 			public void addInformation(ItemStack stack, @Nullable World worldIn, List<String> tooltip, ITooltipFlag flagIn) {
